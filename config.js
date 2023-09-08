c26abb3445783d79dd00d7a4b8f645f8a5cbd6ad
DavidDzunggg <144402117+DavidDzunggg@users.noreply.github.com>
2023-09-08 08:35:29 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/huyen.do1509';
+const eventName = 'Happy Brithday My Honey!!!';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'huyenxinh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
